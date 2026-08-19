506f74ffbe0a547f6f1ae86ace7bd6a9195455e0
Premise18 <promisengeno@gmail.com>
2026-08-19 13:37:04 +0200

    Startscene

--- Assets/Scripts/Menus/StartMenu.cs
+++ Assets/Scripts/Menus/StartMenu.cs
@@ -3,51 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    [Header("Scene to load on Play")]
-    [Tooltip("Must be added to File > Build Settings > Scenes In Build")]
-    public string gameSceneName = "MainGame";
-
-    [Header("Optional Panels")]
-    public GameObject mainPanel;     // Play / Options / Quit buttons
-    public GameObject optionsPanel;  // shown when Options is clicked
-
-    void Start()
+    public void Play()
     {
-        if (mainPanel != null) mainPanel.SetActive(true);
-        if (optionsPanel != null) optionsPanel.SetActive(false);
-
-        Time.timeScale = 1f; // in case a paused game returned to this menu
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+        SceneManager.LoadScene("GameScene");
+        Time.timeScale = 1f;
     }
 
-    // Hook this up to your "Play" button's OnClick
-    public void PlayGame()
+    public void Tutorial(string sceneName)
     {
-        SceneManager.LoadScene(gameSceneName);
+        SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1f;
+
     }
 
-    // Hook this up to your "Options" button's OnClick
-    public void OpenOptions()
-    {
-        if (mainPanel != null) mainPanel.SetActive(false);
-        if (optionsPanel != null) optionsPanel.SetActive(true);
-    }
 
-    // Hook this up to the Options panel's "Back" button OnClick
-    public void CloseOptions()
+    public void Quit()
     {
-        if (optionsPanel != null) optionsPanel.SetActive(false);
-        if (mainPanel != null) mainPanel.SetActive(true);
-    }
-
-    // Hook this up to your "Quit" button's OnClick
-    public void QuitGame()
-    {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
         Application.Quit();
-#endif
     }
 }
---
+++ Assets/Scripts/Menus/UIManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onPlayButtonClicked()
+    {
+        PlayerPrefs.SetString("Scene to go to", "Level - Sabrina");
+        SceneManager.LoadScene("Level - Sabrina");
+    }
+}
